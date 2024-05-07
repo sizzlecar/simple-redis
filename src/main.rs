bc8d80e96ba6677470b3a69cf20a048746d5237b
sizzlecar <pandaathome5479@gmail.com>
2024-05-07 09:03:00 +0800

    feat: listener 6379 and return ok

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,37 @@
-fn main() {
-    println!("Hello, world!");
+use tokio::io::{AsyncReadExt, AsyncWriteExt};
+use tokio::net::TcpListener;
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let listener = TcpListener::bind("127.0.0.1:6379").await?;
+
+    loop {
+        let (mut socket, addr) = listener.accept().await?;
+        println!("Accepted connection from: {}", addr);
+        tokio::spawn(async move {
+            let mut buf = [0; 1024];
+
+            // In a loop, read data from the socket and write the data back.
+            loop {
+                match socket.read(&mut buf).await {
+                    // socket closed
+                    Ok(n) if n == 0 => return,
+                    // 将接受到的数据打印为字符串
+                    Ok(n) => {
+                        let s = String::from_utf8_lossy(&buf[0..n]);
+                        println!("Received: {}", s);
+                        // Write the data back
+                        if let Err(e) = socket.write_all(b"+OK\r\n").await {
+                            eprintln!("failed to write to socket; err = {:?}", e);
+                            return;
+                        }
+                    }
+                    Err(e) => {
+                        eprintln!("failed to read from socket; err = {:?}", e);
+                        return;
+                    }
+                };
+            }
+        });
+    }
 }
